c695585b472eb0c5fa736acc7c7fa7c91b46d5ed
Nate Geslin <teamtomkins23@gmail.com>
2023-12-07 22:12:47 +0000

    Update 5 files

--- src/UsStatesMap.tsx
+++ src/UsStatesMap.tsx
@@ -1,16 +1,35 @@
 import * as d3 from "d3";
 import { useEffect, useRef } from "react";
+import * as usStates from "../public/data/us.json";
+import * as topojson from "topojson";
+
+function onLoadUsStates(svg, path) {
+  const states = topojson.feature(usStates, usStates.objects.states).features;
+
+  svg
+    .selectAll(".state")
+    .data(states)
+    .enter()
+    .append("path")
+    .attr("class", "state")
+    .attr("d", path);
+}
 
 export function UsStatesMap() {
   const ref = useRef();
 
   useEffect(() => {
-    // set the dimensions and margins of the graph
-    const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+    const margin = {
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 10,
+    };
+    const xMargin = margin.left + margin.right;
+    const yMargin = margin.top + margin.bottom;
+    const width = 800 - xMargin;
+    const height = 400 - yMargin;
 
-    // append the svg object to the body of the page
     const svg = d3
       .select(ref.current)
       .append("svg")
@@ -19,40 +38,14 @@ export function UsStatesMap() {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Parse the Data
-    d3.csv(
-      "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv",
-    ).then(function (data) {
-      // X axis
-      const x = d3
-        .scaleBand()
-        .range([0, width])
-        .domain(data.map((d) => d.Country))
-        .padding(0.2);
-      svg
-        .append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+    const projection = d3
+      .geoAlbersUsa()
+      .translate([width / 2, height / 2])
+      .scale(850);
+    const path = d3.geoPath().projection(projection);
 
-      // Add Y axis
-      const y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
-
-      // Bars
-      svg
-        .selectAll("mybar")
-        .data(data)
-        .join("rect")
-        .attr("x", (d) => x(d.Country))
-        .attr("y", (d) => y(d.Value))
-        .attr("width", x.bandwidth())
-        .attr("height", (d) => height - y(d.Value))
-        .attr("fill", "#5f0f40");
-    });
+    onLoadUsStates(svg, path);
   }, []);
 
-  return <svg width={460} height={400} id="barchart" ref={ref} />;
+  return <svg width={800} height={400} id="us-states-map" ref={ref} />;
 }
